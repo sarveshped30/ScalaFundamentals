5b434228780e620badc06b7dac729978ebbfcece
sarvesh pednekar <sarveshpednekar30@gmail.com>
2025-04-10 23:04:41 +0530

    Scala:Fundamentals (String function and interpolation)

---
+++ src/main/scala/StringsFunctions.scala
@@ -0,0 +1,22 @@
+
+
+@main
+def stringFunctions(): Unit = {
+
+  val myPet = "Crocodile pednekar";
+  println(myPet.length);
+
+  println(myPet.toLowerCase())  //De-capitalize whole String
+  println(myPet.toUpperCase())  //Capitalize whole string
+  println(myPet.capitalize)     //Capitalize first letter of string
+  println(myPet.trim)           //Trims Left most and right most spaces
+  println(myPet.charAt(2))      //Get char at index
+  println(myPet(2))             //Scala way to get char on index
+  println(myPet.substring(2))   //Start inclusive
+  println(myPet.substring(2, 7))  // Start inclusive and end exclusive
+
+  //Interpolation
+  println(s"My Pet name is $myPet");
+  println(s"My Pet name is ${myPet.split(" ").head} and surname is ${myPet.split(" ").tail.head}");
+
+}
