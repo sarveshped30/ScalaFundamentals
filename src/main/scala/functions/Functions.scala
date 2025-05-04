39a110a6426a70119de1672bb8c0d0ee5608c051
sarvesh pednekar <sarveshpednekar30@gmail.com>
2025-05-04 21:37:23 +0530

    Scala:Fundamentals (Functions Overloading)

--- src/main/scala/functions/Functions.scala
+++ src/main/scala/functions/Functions.scala
@@ -5,10 +5,15 @@ import scala.io.StdIn
 @main
 def init(): Unit = {
 
-  print("Enter animal name : ")
+/*  print("Enter animal name : ")
   val animal = StdIn.readLine()
   val lifeSpan = checkLifeSpan(animal)
-  println(s"\nLife span of $animal is $lifeSpan")
+  println(s"\nLife span of $animal is $lifeSpan")*/
+
+  val products = Map("Smartphone" -> 45_678D, "Tablet" -> 30_000D, "Laptop" -> 1_00_000D)
+
+  addTax("Laptop", 1_00_000)
+  addTax(products)
 
 }
 
@@ -19,4 +24,14 @@ def checkLifeSpan(animal: String): Int = {
     lifeSpan = animals(animal)
   }
   lifeSpan
+}
+
+//Function overloading (compile time polymorphism)
+
+def addTax(product: String, prize: Double): Unit =  println(s"$product prize after applying 20% tax is ${prize * 1.20} Rs")
+
+def addTax(products: Map[String, Double]): Unit = {
+  for(product <- products.keySet) {
+    println(s"$product prize after applying 20% tax is ${products(product) * 1.20} Rs")
+  }
 }
